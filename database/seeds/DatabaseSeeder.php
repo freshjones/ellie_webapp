2401de83d8167c8865ce2fbaed57aeffd732b324
William Jones <billy@freshjones.com>
2015-01-27 10:54:55 -0500

    adding variables seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,24 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('VariablesTableSeeder');
+	}
+
+}
+
+class VariablesTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('variables')->delete();
+
+		$variables = array();
+		$variables[] = ['name' => 'site_name', 'value' => getenv(SITE_NAME),];
+		$variables[] = ['name' => 'site_color', 'value' => getenv(SITE_COLOR),];
+		$variables[] = ['name' => 'site_template', 'value' => getenv(SITE_TEMPLATE),];
+
+		DB::table('variables')->insert($variables);
+
 	}
 
 }
